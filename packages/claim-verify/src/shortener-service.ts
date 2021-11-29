3bec6c80509c2e27314def9b0089c9d8f240e60e
Mirko Mollik <mollik@trustcerts.de>
2021-11-29 18:44:33 +0100

    feat: add shorten service

---
+++ packages/claim-verify/src/shortener-service.ts
@@ -0,0 +1,28 @@
+import {
+  Configuration,
+  ShortenPlatformApi,
+} from '@trustcerts/core/dist/platform';
+
+/**
+ * Shortes a url
+ */
+export class ShortenerService {
+  private api: ShortenPlatformApi;
+
+  constructor(url: string) {
+    const configuration = new Configuration({
+      basePath: url,
+    });
+    this.api = new ShortenPlatformApi(configuration);
+  }
+
+  public shorten(url: string, iv: string): Promise<string> {
+    return this.api
+      .shortenControllerShorten({ url, iv })
+      .then(res => res.data.url);
+  }
+
+  public resolve(id: string) {
+    return this.api.shortenControllerResolve(id).then(res => res.data);
+  }
+}
